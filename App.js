d6531761609299edea86f938a2a2fee25e45ac89
IsmaCortGtz <ismacortgtz@gmial.com>
2022-07-26 15:24:41 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WebView } from 'react-native-webview';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Stack.Screen name="Radar Doppler GDL" component={Test}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Test(){
+
+  const webViewRef = useRef();
+
+  return (
+    <View style={{flex: 1}}>
+      <WebView
+        ref={(ref) => webViewRef.current = ref}
+        source={{uri: "http://iam.cucei.udg.mx/radar/iam/"}}
+        injectedJavaScript={""}
+      />
+      <Button title='Recargar' onPress={() => webViewRef.current.reload()}/>
+    </View>
+  );
+}
